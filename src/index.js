e4d06f65f9a0bc4b3978169321e2b552d6e06857
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2019-04-11 19:47:35 +0100

    added basic state

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  return <div>Hi There!</div>;
-};
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+
+    // the only time that there is direct assignment to this.state
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // setState is always what is used to update state!
+        this.setState({ lat: position.coords.latitude })
+      },
+      (err) => console.log(err)
+    );
+  }
+
+  // React says we have to define render
+  render () {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
 
 ReactDOM.render(
   <App />,
